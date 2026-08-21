a26ee5db700b373adf2521a2a9be8f4c2b5cd1e2
KarimTamer74 <kemotamer448@gmail.com>
2026-08-21 15:41:58 +0300

    create app styles

---
+++ lib/app_styles.dart
@@ -0,0 +1,10 @@
+// app_styles.dart
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle style18Bold = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle style12 = TextStyle(fontSize: 12);
+}
